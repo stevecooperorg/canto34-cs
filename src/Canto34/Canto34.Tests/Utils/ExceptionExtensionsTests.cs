325ae25f1dc813ccdcbe9f5376f2278136fdb497
Steve Cooper <steve@stevecooper.org>
2014-05-01 23:32:39 +0100

    added a failing test for extension stack trace rewriting

---
+++ src/Canto34/Canto34.Tests/Utils/ExceptionExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Canto34.Utils;
+
+namespace Canto34.Tests.Utils
+{
+    [TestClass]
+    public class ExceptionExtensionsTests
+    {
+        [TestMethod]
+        public void ShouldThrowExceptionProperly()
+        {
+            try
+            {
+                throw new Exception("Foo");
+            }
+            catch (Exception ex)
+            {
+                ex.ImpersonateErrorInExternalFile(@"c:\foo\bar.txt", 99);
+
+                var stackTrace = ex.StackTrace;
+
+                var hasFileReference = stackTrace.EndsWith(@"in c:\foo\bar.txt:line 99");
+                Assert.IsTrue(hasFileReference);
+            }
+        }
+    }
+}
